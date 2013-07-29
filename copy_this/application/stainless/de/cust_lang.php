106de1c487a783836837d876ac14eb37ddf8290a
Johannes Ackermann <ja@imva.biz>
2013-07-29 19:49:33 +0200

    Added Template Files
    Azure Theme of the OXID eShop Community Edition with some preparation;
    deleted original CSS, changed language files.

---
+++ copy_this/application/stainless/de/cust_lang.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * This file is part of the Stainless Theme for the OXID eShop.
+ *
+ * This theme is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The Stainless Theme is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OXID eShop Community Edition.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @link		http://imva.biz/stainless
+ * @copyright	(C) imva.biz
+ * @version		0.1
+ */
+
+$sLangName  = "Deutsch";
+// -------------------------------
+// RESOURCE IDENTIFIER = STRING
+// -------------------------------
+$aLang = array(
+
+'charset'                                  => 'ISO-8859-15',
+
+);
+
+/*
+[{ oxmultilang ident="GENERAL_YOUWANTTODELETE" }]
+*/
